8f49e1c963e674b711fa886d18b892b49b652ad6
George Roberts <gr@gr5.org>
2017-02-03 23:01:39 -0500

    Made it so these constants which change depending on printer and configuration can be set in package.sh

--- Marlin/wattage_distribution.h
+++ Marlin/wattage_distribution.h
@@ -1,10 +1,18 @@
 #ifndef WATTAGEDIST_H
 #define WATTAGEDIST_H
 
-// default values for wattage distribution
-#define DEFAULT_WATTAGE_BUDGET     190
-#define DEFAULT_WATTAGE_EXTRUDER    35
-#define DEFAULT_WATTAGE_BUILDPLATE 150
+// default values for wattage distribution for all but um2go
+#ifndef DEFAULT_WATTAGE_BUDGET
+    #define DEFAULT_WATTAGE_BUDGET     190
+#endif
+
+#ifndef DEFAULT_WATTAGE_EXTRUDER
+    #define DEFAULT_WATTAGE_EXTRUDER    35
+#endif
+
+#ifndef DEFAULT_WATTAGE_BUILDPLATE
+    #define DEFAULT_WATTAGE_BUILDPLATE 150
+#endif
 
 // min/max allowed input value
 #define WATTAGE_MINVALUE             0
